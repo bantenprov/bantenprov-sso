8c1a235029d2301f00ee5e95ab28f33d3fa30081
ahmadnorin <ahmadnorin@gmailcom>
2017-11-24 22:12:53 +0700

    update fix

---
+++ src/LaravelSsoClient.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @Author: ahmadnorin
+ * @Date:   2017-11-24 22:07:11
+ * @Last Modified by:   ahmadnorin
+ * @Last Modified time: 2017-11-24 22:07:59
+ */
+namespace Bantenprov\LaravelSsoClient;
+
+class LaravelSsoClient
+{
+	static function index()
+	{
+		return 'package loaded';
+	}
+}
